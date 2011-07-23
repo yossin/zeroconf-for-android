cf6c90ed194b6cd1fe012914425e8fb0d429255e
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-07-23 22:51:31 +0000

    add providers for devices

---
+++  zeroconf-for-android/jmdns-ws/providers/mta/yos/zeroconf/providers/JavaLampProvider.java
@@ -0,0 +1,32 @@
+package mta.yos.zeroconf.providers;
+
+import mta.yos.zeroconf.devices.DeviceProvider;
+import mta.yos.zeroconf.domain.Device;
+
+public class JavaLampProvider implements DeviceProvider{
+
+	private Device device;
+	
+	public JavaLampProvider(Device device) {
+		this.device = device;
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int status() {
+		// TODO Auto-generated method stub
+		return 10;
+	}
+
+}
