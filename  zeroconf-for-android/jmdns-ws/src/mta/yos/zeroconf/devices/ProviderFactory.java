df2f08d4b8c40e6ad01dceb5f5dc4ad46034f2f7
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-07-28 03:58:17 +0000

    initialize a provider with hostname & port instead of a device

---  zeroconf-for-android/jmdns-ws/src/mta/yos/zeroconf/devices/ProviderFactory.java
+++  zeroconf-for-android/jmdns-ws/src/mta/yos/zeroconf/devices/ProviderFactory.java
@@ -7,9 +7,9 @@ import mta.yos.zeroconf.domain.Device;
 public class ProviderFactory {
 	public DeviceProvider create(Device device) throws Exception{
 		Class<DeviceProvider> clazz = (Class<DeviceProvider>) Class.forName(device.getService().getProviderClassName());
-		Class<?> parameterTypes[] = new Class[]{Device.class};
+		Class<?> parameterTypes[] = new Class[]{String.class, Integer.TYPE};
 		Constructor<DeviceProvider> constructor = clazz.getConstructor(parameterTypes);
-		Object initargs[] = new Object[]{device};
+		Object initargs[] = new Object[]{device.getService().getHostname(), device.getService().getPort()};
 		return constructor.newInstance(initargs);
 	}
 }
