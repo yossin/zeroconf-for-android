4b61700f06a93cc931dacfe99e1aeaa1e83768ac
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-07-28 03:56:49 +0000

    pass hostname & port instead of a device

---  zeroconf-for-android/jmdns-ws/providers/mta/yos/zeroconf/providers/JavaLampProvider.java
+++  zeroconf-for-android/jmdns-ws/providers/mta/yos/zeroconf/providers/JavaLampProvider.java
@@ -1,32 +1,72 @@
 package mta.yos.zeroconf.providers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import mta.yos.zeroconf.devices.DeviceProvider;
-import mta.yos.zeroconf.domain.Device;
 
 public class JavaLampProvider implements DeviceProvider{
 
-	private Device device;
+	String hostname;
+	int port;
 	
-	public JavaLampProvider(Device device) {
-		this.device = device;
+	public JavaLampProvider(String hostname, int port) {
+		this.hostname= hostname;
+		this.port= port;
+	}
+	
+	private String sendReceive(String message) throws IOException{
+		Socket socket=null;
+		PrintWriter out=null;
+		BufferedReader in=null;
+		String response=null;
+		try {
+		socket = new Socket(hostname, port);
+		out = new PrintWriter(socket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		// send
+		out.println(message);
+		out.flush();
+		// receive
+		response = in.readLine();
+		} finally {
+			try {
+				if (in!=null) in.close();
+			} catch (IOException e) {
+			}
+			if (out!=null) out.close();
+			try {
+				if (socket!=null) socket.close();
+			} catch (IOException e) {
+			}
+		}
+		
+		return response;
+
 	}
 
 	@Override
-	public void turnOff() {
-		// TODO Auto-generated method stub
+	public void turnOff() throws IOException{
+		sendReceive("turnOff");
 		
 	}
 
 	@Override
-	public void turnOn() {
-		// TODO Auto-generated method stub
-		
+	public void turnOn() throws IOException{
+		sendReceive("turnOn");
 	}
 
 	@Override
-	public int status() {
-		// TODO Auto-generated method stub
-		return 2;
+	public int status() throws IOException{
+		String status=sendReceive("status");
+		try {
+			return Integer.parseInt(status);
+		} catch (IllegalArgumentException e){
+			throw new IOException("unable to receive status (status is: "+ status+")");
+		}
 	}
 
 }
