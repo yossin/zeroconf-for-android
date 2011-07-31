a30f1a2c09aff5d495560289af22d1830299e0ce
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-07-31 05:00:28 +0000

    add serial number for id persistence

---  zeroconf-for-android/lamps/src/mta/yos/zeroconf/lamp/DeviceRegister.java
+++  zeroconf-for-android/lamps/src/mta/yos/zeroconf/lamp/DeviceRegister.java
@@ -1,8 +1,5 @@
 package mta.yos.zeroconf.lamp;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-
 import com.apple.dnssd.DNSSD;
 import com.apple.dnssd.DNSSDException;
 import com.apple.dnssd.DNSSDRegistration;
@@ -15,6 +12,7 @@ class DeviceRegister {
 	final static String type="_device._tcp";
 	int basePort;
 	String name;
+	String serialNumber;
 	String providerClassName;
 
 	public static class Listener implements RegisterListener{
@@ -35,38 +33,28 @@ class DeviceRegister {
 	RegisterListener listener = new Listener();
 	DNSSDRegistration registration;
 	
-	public DeviceRegister(String name, int basePort, String providerClassName) {
+	public DeviceRegister(String name, String serialNumber, int basePort, String providerClassName) {
 		this.name = name;
+		this.serialNumber = serialNumber;
 		this.basePort= basePort;
 		this.providerClassName = providerClassName;
-	}
-
-	private static String getMacAddress() throws Exception{
-		InetAddress local = InetAddress.getLocalHost();
-		NetworkInterface ni = NetworkInterface.getByInetAddress(local);
-		byte []mac = ni.getHardwareAddress();
-		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < mac.length; i++) {
-			String m=String.format("%02X%s", mac[i], (i < mac.length - 1) ? ":" : "");
-			builder.append(m);
-		}
-		return builder.toString();
 	}
 
 	private int calculateId(int listenPort){
 		return listenPort-basePort+1;
 	}
 	
-	private TXTRecord createRecord(int id) throws DNSSDException{
+	private TXTRecord createRecord() throws DNSSDException{
 		TXTRecord record = new TXTRecord();
-		record.set("deviceName", name+id);
+		record.set("deviceName", name+serialNumber);
 		record.set("providerClassName", providerClassName);
+		record.set("serialNumber", serialNumber);
 		return record;
 	}
 	public void register(int listenPort) throws Exception{
 		int id = calculateId(listenPort);
-		String serviceName=name+id+"["+getMacAddress()+"]";
-		TXTRecord record  = createRecord(id);
+		String serviceName=name+id+"["+serialNumber+"]";
+		TXTRecord record  = createRecord();
 		int ifIndex=0;
 		int flags=DNSSD.DEFAULT;
 		registration = DNSSD.register(flags, ifIndex, serviceName, type, null, null, listenPort, record, listener);
