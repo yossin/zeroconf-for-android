060a86953479becd5854a984b3b111354e2236da
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-07-31 04:59:26 +0000

    extract app arguments as variables

--- trunk/ zeroconf-for-android/lamps/src/mta/yos/zeroconf/lamp/ConsoleLamp.java
+++ trunk/ zeroconf-for-android/lamps/src/mta/yos/zeroconf/lamp/ConsoleLamp.java
@@ -3,8 +3,13 @@ package mta.yos.zeroconf.lamp;
 
 public class ConsoleLamp {
 
+	
 	public static void main(String[] args) throws Exception {
-		LampApp app = new LampApp(new ConsoleLampImpl(), "ConsoleLamp",9009,"mta.yos.zeroconf.providers.JavaLampProvider");
+		String name="ConsoleLamp";
+		int port=9009;
+		String provider="mta.yos.zeroconf.providers.JavaLampProvider";
+		String serialNumber="123321";
+		LampApp app = new LampApp(new ConsoleLampImpl(),name , serialNumber, port, provider);
 		app.run();
 	}
 }
