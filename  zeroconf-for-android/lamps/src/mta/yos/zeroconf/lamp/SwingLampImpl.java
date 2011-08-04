1a018f91908a904fb5316c269cad645ace0cf7bf
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-08-04 03:21:09 +0000

    handle frame instead of button

---  zeroconf-for-android/lamps/src/mta/yos/zeroconf/lamp/SwingLampImpl.java
+++  zeroconf-for-android/lamps/src/mta/yos/zeroconf/lamp/SwingLampImpl.java
@@ -1,12 +1,11 @@
 package mta.yos.zeroconf.lamp;
 
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class SwingLampImpl implements Lamp {
@@ -53,40 +52,51 @@ public class SwingLampImpl implements Lamp {
 	@Override
 	public synchronized void turnOff() {
 		status=0;
-		btn.setBackground(Color.darkGray);
-		btn.setText("Off");
+		frame.getContentPane().setBackground(Color.darkGray);
+		frame.setTitle("Off");
 	}
 
 	@Override
 	public synchronized void turnOn() {
 		status=1;
-		btn.setBackground(Color.yellow);
-		btn.setText("On");
+		frame.getContentPane().setBackground(Color.yellow);
+		frame.setTitle("On");
 	}
 	final JFrame frame= new JFrame("Swing Lamp");
-    final JButton btn = new JButton();
 
 	@Override
 	public void display() {
 	    turnOff();
+	    
 	    frame.addWindowListener(new Listener());
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setBounds(80,80,100,100);
+	    frame.setBounds(80,80,160,160);
 	    frame.setResizable(false);
-	    frame.setVisible(true);
-	    btn.addActionListener(new ActionListener() {
-			
+	    frame.addMouseListener(new MouseListener() {
 			@Override
-			public void actionPerformed(ActionEvent event) {
+			public void mouseClicked(MouseEvent arg0) {
 				if(status()==0){
 					turnOn();
 				} else {
 					turnOff();
 				}
-				
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+			}
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+			}
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+			}
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
 			}
 		});
-	    frame.add(btn);
+	    frame.setVisible(true);
+
 	}
 
 	LampListener listener;
