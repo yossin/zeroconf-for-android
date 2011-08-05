5d291c8dd1020ce4eb2cdeb2ad7443776034a914
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-08-05 12:03:37 +0000

    remove providers

---  zeroconf-for-android/jmdns-ws/src/mta/yos/zeroconf/devices/DeviceProvider.java
+++
@@ -1,9 +0,0 @@
-package mta.yos.zeroconf.devices;
-
-import java.io.IOException;
-
-public interface DeviceProvider {
-	void turnOff() throws IOException;
-	void turnOn() throws IOException;
-	int status() throws IOException;
-}
