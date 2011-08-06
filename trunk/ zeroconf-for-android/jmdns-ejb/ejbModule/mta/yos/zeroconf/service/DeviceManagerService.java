e5c0fd00bab7147ca69c0017c4cc44ed2046d2ed
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-08-06 21:21:14 +0000

    support device delete on service

--- trunk/ zeroconf-for-android/jmdns-ejb/ejbModule/mta/yos/zeroconf/service/DeviceManagerService.java
+++ trunk/ zeroconf-for-android/jmdns-ejb/ejbModule/mta/yos/zeroconf/service/DeviceManagerService.java
@@ -13,5 +13,6 @@ public interface DeviceManagerService {
 	List<Device> deviceList() throws Exception;
 	void assignDeviceToZone(@WebParam(name="deviceId")String deviceId, @WebParam(name="zoneName")String zoneName) throws Exception;
 	void createZone(@WebParam(name="zone")Zone zone) throws Exception;
+	void deleteDevice(@WebParam(name="deviceId")String deviceId) throws Exception;
 	void updateDeviceRadius(@WebParam(name="deviceId")String deviceId, @WebParam(name="radius") long radius) throws Exception;
 }
