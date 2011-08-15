f1aa64eae27a1030b8e5f61be92179cc71b0a4c4
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-08-15 05:18:05 +0000

    1. support forwarding provider 2. shutdown properly from linux

---  zeroconf-for-android/lamps/src/mta/yos/zeroconf/lamp/JmdnsSwingLamp.java
+++  zeroconf-for-android/lamps/src/mta/yos/zeroconf/lamp/JmdnsSwingLamp.java
@@ -7,12 +7,14 @@ public class JmdnsSwingLamp {
 	public static void main(String[] args) throws Exception {
 		String name="JmdnsSwingLamp";
 		int port=7007;
-		String provider="mta.yos.zeroconf.providers.JavaLampProvider";
+		String provider="mta.yos.zeroconf.providers.JavaLampForwardingProvider";
 		String serialNumber="123454321";
 		LampInfo info = new LampInfo(name, port, provider, serialNumber);
 		BaseLampApp app = new JmdnsLampApp(info);
 		Lamp lamp = new SwingLampImpl();
 		lamp.setListener(app);
 		lamp.display();
+		// wait for unregistration
+		//Thread.sleep(10000);
 	}
 }
