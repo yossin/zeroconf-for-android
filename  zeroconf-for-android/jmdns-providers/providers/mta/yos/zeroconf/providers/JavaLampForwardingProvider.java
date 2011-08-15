09a9b48e4919b9f31d1b0486e02dc36be16ab80d
josefn.il@gmail.com <josefn.il@gmail.com@73545f29-6ecd-f579-c9e3-16bdc52d5d20>
2011-08-15 05:21:10 +0000

    support forwarding provider

---
+++  zeroconf-for-android/jmdns-providers/providers/mta/yos/zeroconf/providers/JavaLampForwardingProvider.java
@@ -0,0 +1,11 @@
+package mta.yos.zeroconf.providers;
+
+
+public class JavaLampForwardingProvider extends JavaLampProvider{
+
+	public JavaLampForwardingProvider(String hostname, int port) {
+		super("localhost", port);
+	}
+	
+
+}
